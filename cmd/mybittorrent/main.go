6054043b943b0cf0b4e49e1c8bd43f06ad601464
Vasu Bansal <vasubansal1998@gmail.com>
2024-07-01 00:48:39 +0530

    [Vasu] refactored to have a switch statement

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -13,34 +13,46 @@ import (
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if rune(bencodedString[0]) == 'i' {
-		res, err := strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-		if err != nil {
-			return "", err
-		}
-
-		return res, nil
-	} else {
+	switch {
+	case unicode.IsDigit(rune(bencodedString[0])):
+		return decodeString(bencodedString)
+	case rune(bencodedString[0]) == 'i':
+		return decodeNumber(bencodedString)
+	default:
 		return "", fmt.Errorf("only strings are supported at the moment")
 	}
+}
+
+func decodeString(bencodedString string) (string, error) {
+	var firstColonIndex int
+
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bencodedString[:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", err
+	}
+	if firstColonIndex+1+length > len(bencodedString) {
+		return "", fmt.Errorf("index %d out of bounds as expected length is: %d but is: %d", length, firstColonIndex+1+length, len(bencodedString))
+	}
+
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+}
+
+func decodeNumber(bencodedString string) (int, error) {
+	res, err := strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
+	if err != nil {
+		panic(err)
+	}
+
+	return res, nil
 }
 
 func main() {
